b0daeaf5e81c31fc1add6f86b5f3222fd8111e0b
adinugro88 <adin5337@gmail.com>
2023-04-27 08:31:19 +0700

    upload data

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', function() {
+    return redirect('/login');
+});
+
+Route::post('/register', function() {
+    return redirect('/login');
+});
 
 
 
